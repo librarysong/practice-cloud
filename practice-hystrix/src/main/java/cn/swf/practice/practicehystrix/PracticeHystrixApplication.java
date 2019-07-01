0dd86f301b0eba4a890b08897ef5084eb0acc258
swf <1101699259@qq.com>
2019-07-01 14:17:44 +0800

    hystrix

---
+++ practice-hystrix/src/main/java/cn/swf/practice/practicehystrix/PracticeHystrixApplication.java
@@ -0,0 +1,13 @@
+package cn.swf.practice.practicehystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeHystrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeHystrixApplication.class, args);
+	}
+
+}
---
+++ practice-hystrix/src/main/java/cn/swf/practice/practicehystrix/config/RestTemplateConfig.java
@@ -0,0 +1,32 @@
+package cn.swf.practice.practicehystrix.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by 宋维飞
+ * 2019/7/1 14:10
+ */
+@Configuration
+@Slf4j
+public class RestTemplateConfig {
+
+    @Bean(name = "httpRequestFactory")
+    public ClientHttpRequestFactory httpRequestFactory(){
+        HttpComponentsClientHttpRequestFactory factory=new HttpComponentsClientHttpRequestFactory();
+        factory.setReadTimeout(3000);
+        factory.setConnectTimeout(3000);
+        log.info("RestTemplateConfig success");
+        return factory;
+    }
+
+    @Bean(name = "restTemplate")
+    public RestTemplate restTemplate(@Qualifier(value = "httpRequestFactory") ClientHttpRequestFactory factory){
+        return new RestTemplate(factory);
+    }
+}
