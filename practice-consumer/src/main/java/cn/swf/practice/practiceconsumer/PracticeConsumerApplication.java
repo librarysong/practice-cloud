4215c3bed4e0b2553612942019cc3504848e5e3f
swf <1101699259@qq.com>
2019-07-01 12:54:14 +0800

    consumer

--- practice-consumer/src/main/java/cn/swf/practice/practiceconsumer/PracticeConsumerApplication.java
+++ practice-consumer/src/main/java/cn/swf/practice/practiceconsumer/PracticeConsumerApplication.java
@@ -3,9 +3,11 @@ package cn.swf.practice.practiceconsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class PracticeConsumerApplication {
 
 	public static void main(String[] args) {
