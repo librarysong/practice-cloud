f48c324bbdb19596791da7fe3715c3ea685b3d12
swf <1101699259@qq.com>
2019-07-08 14:28:22 +0800

    add practice-master

---
+++ practice-master/src/main/java/cn/swf/practice/practicemaster/PracticeMasterApplication.java
@@ -0,0 +1,13 @@
+package cn.swf.practice.practicemaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeMasterApplication.class, args);
+	}
+
+}
---
+++ practice-master/src/test/java/cn/swf/practice/practicemaster/PracticeMasterApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.swf.practice.practicemaster;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PracticeMasterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
